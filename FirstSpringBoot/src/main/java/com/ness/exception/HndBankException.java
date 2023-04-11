863db9ec0d02479f8ced1ba5f43e9cbc276d77e6
harshada <harshadasawant22@gmail.com>
2023-04-11 10:30:43 +0530

    Added Customer Login functionality

---
+++ FirstSpringBoot/src/main/java/com/ness/exception/HndBankException.java
@@ -0,0 +1,8 @@
+package com.ness.exception;
+
+public class HndBankException extends RuntimeException
+{
+    public HndBankException(String mesg){
+        super(mesg);
+    }
+}
---
+++ FirstSpringBoot/src/main/java/com/ness/service/CustomerLoginServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ness.service;
+
+import com.ness.dto.CustomerLoginDTO;
+import com.ness.exception.HndBankException;
+import com.ness.repository.CustomerLoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service(value="customerLoginService")
+public class CustomerLoginServiceImpl implements CustomerLoginService {
+    @Autowired
+    private CustomerLoginRepository customerLoginRepository;
+    public String authenticateCustomer(CustomerLoginDTO customerLogin) throws HndBankException {
+        String toRet = null;
+        CustomerLoginDTO customerLoginFromRepository = customerLoginRepository
+                .getCustomerLoginByLoginName(customerLogin.getLoginName());
+        if (customerLogin.getPassword().equals(customerLoginFromRepository.getPassword())){
+            toRet = "SUCCESS";
+        }else{
+            throw new HndBankException("Service.WRONG_CREDENTIALS");
+        }
+        return toRet;
+    }
+}
